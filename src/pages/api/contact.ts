fc4b84212002d092b624254f409401ed224713cc
fullstackNRJ <niraj.mukta123@gmail.com>
2025-05-07 17:11:41 +0530

    made necessary changes

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -6,7 +6,7 @@ export const POST: APIRoute = async ({ request }) => {
     const data = await request.json();
     
     // Validate form data
-    if (!data.name || !data.email || !data.message) {
+    if (!data.name || !data.email || !data.subject) {
       return new Response(
         JSON.stringify({
           message: 'Missing required fields',
@@ -17,14 +17,39 @@ export const POST: APIRoute = async ({ request }) => {
     
     // In a real app, you would send an email, store in database, etc.
     console.log('Contact form submission:', data);
+
+
+      const response = await fetch('https://app.nocodb.com/api/v2/tables/m8x5b3n4xsnu2t7/records',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'xc-token': 'HU87soieXeH9e1hgE1SS5THFWCxGaWJFr3mIc6bF' //move to env file
+        },
+        body: JSON.stringify({
+        
+            "Name": data.name,
+            "Email": data.email,
+            "Subject": data.subject,
+            "Message": data.message
+          
+        })
+      })
+
+      if(!response.ok){
+        throw new Error('Failed to save lead data');
+      }
+
+      const result = await response.json();
+
+      return new Response(
+        JSON.stringify({
+          message: 'Message sent successfully!',
+          result
+        }),
+        { status: 200 }
+      );
+
     
-    // Return success response
-    return new Response(
-      JSON.stringify({
-        message: 'Message sent successfully!',
-      }),
-      { status: 200 }
-    );
   } catch (error) {
     console.error('Error processing contact form:', error);
     
