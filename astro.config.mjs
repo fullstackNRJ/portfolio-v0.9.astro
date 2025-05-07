28bc227c140656fc8a1994b662de0d9e51e32046
fullstackNRJ <niraj.mukta123@gmail.com>
2025-05-07 17:37:36 +0530

    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +11,6 @@ export default defineConfig({
     react()
   ],
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: cloudflare()
+  
 });
